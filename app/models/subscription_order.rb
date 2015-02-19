8022cadbd5596e46853027534571cc82cbe09b0a
shinofumijp <electric.spanish.shinofumi.335@gmail.com>
2015-02-20 01:16:34 +0900

    Remove unneeded model

--- app/models/subscription_order.rb
+++ app/models/subscription_order.rb
@@ -1,4 +1,5 @@
 class SubscriptionOrder < ActiveRecord::Base
   belongs_to :purchase_order
   belongs_to :variant
+  has_many :subscription_order_details
 end
--- spec/factories/bills_order_details.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :bills_order_detail do
-    bill nil
-order_detail_id 1
-order_detail_type "MyString"
-  end
-
-end
