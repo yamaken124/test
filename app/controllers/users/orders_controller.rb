631e471b06994368d823e2426373d47612afa2d1
otani shinji <otanishinji@otani-no-MacBook-Air.local>
2015-03-13 01:26:28 +0900

    Delete unneeded includes

--- app/controllers/users/orders_controller.rb
+++ app/controllers/users/orders_controller.rb
@@ -11,9 +11,6 @@ class Users::OrdersController < Users::BaseController
   def thanks
     @number = params[:number]
     raise ActiveRecord::RecordNotFound if !Payment.where(number: @number).first.completed?
-    @detail = SingleOrderDetail
-    .where(id: Payment.where(number: @number).first.single_order_detail_id)
-    .includes(:single_line_items).first
     set_item_and_prouct
   end
 
