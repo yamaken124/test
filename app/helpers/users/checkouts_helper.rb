0606237ec8c330acde18f8d233defedf24eb18a4
otani shinji <otanishinji@otani-no-MacBook-Air.local>
2015-03-12 19:12:28 +0900

    Register payment_method_id

--- app/helpers/users/checkouts_helper.rb
+++ app/helpers/users/checkouts_helper.rb
@@ -1,5 +1,5 @@
 module Users::CheckoutsHelper
   def permitted_checkout_attributes
-    [{ payment_attributes: [:gmo_card_seq_temporary, :address_id] }, :used_point]
+    [{ payment_attributes: [:gmo_card_seq_temporary, :address_id, :payment_method_id] }, :used_point]
   end
 end
