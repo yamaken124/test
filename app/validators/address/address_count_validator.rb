cd070f55aff084eb151fde84b5283183385ae80b
otani shinji <otanishinji@otani-no-MacBook-Air.local>
2015-03-16 00:09:22 +0900

    Reset arguments user

--- app/validators/address/address_count_validator.rb
+++ app/validators/address/address_count_validator.rb
@@ -2,7 +2,7 @@ class Address < ActiveRecord::Base
   class AddressCountValidator < ActiveModel::Validator
 
     def validate(record)
-      if Address.reach_upper_limit?(record)
+      if Address.reach_upper_limit?(record.user)
         record.errors.add(:base, I18n.t("validate_registration.address"))
       end
     end
