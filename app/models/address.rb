f4ea29ee81d67242f804e2d73e232cc0583d895f
otani shinji <otanishinji@otani-no-MacBook-Air.local>
2015-03-15 23:59:15 +0900

    Set argument user

--- app/models/address.rb
+++ app/models/address.rb
@@ -28,8 +28,8 @@ class Address < ActiveRecord::Base
 
   UpperLimit = 3
 
-  def self.reach_upper_limit?(user_id)
-    self.where(user_id: user_id).count >= UpperLimit
+  def self.reach_upper_limit?(user)
+    self.where(user_id: user.id).count >= UpperLimit
   end
 
 end
