c8e8dc5bbeb5703d5148dd92ec41f882acc6efff
shinofumijp <electric.spanish.shinofumi.335@gmail.com>
2015-03-17 19:38:40 +0900

    Embed staging ip addr

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,13 +7,13 @@ set :unicorn_rack_env, 'production'
 set :bundle_without,  [:development, :test, :heroku_staging]
 
 set :application_server_addrs, [
-  ''
+  '54.178.185.30'
 ]
 
 set :cron_server_addr, fetch(:application_server_addrs).first
 set :db_server_addr, fetch(:cron_server_addr)
 
-fetch(:application_addrs).each do |server_addr|
+fetch(:application_server_addrs).each do |server_addr|
   server server_addr, user: 'ubuntu', roles: %w(app web worker)
 end
 server fetch(:cron_server_addr), user: 'ubuntu', roles: %w(cron web)
