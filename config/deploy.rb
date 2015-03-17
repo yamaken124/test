5c65768b1dd4f697069c6d8fb4eb90c42a03e84d
shinofumijp <electric.spanish.shinofumi.335@gmail.com>
2015-03-17 22:56:02 +0900

    Remove symlink to database.yml & secrets.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :deploy_via, :remote_cache
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
@@ -46,8 +46,5 @@ namespace :deploy do
       # end
     end
   end
-
-  before :starting, 'deploy:linked_files:database'
-  before :starting, 'deploy:linked_files:secrets'
 
 end
