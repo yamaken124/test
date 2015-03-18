0420cd854c9a9669e9c34feed9fc7fb12372c943
shinofumijp <electric.spanish.shinofumi.335@gmail.com>
2015-03-18 22:23:50 +0900

    Simplify carrirewave image path

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,11 +9,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    if Rails.env.development?
-      "uploads/tmp/#{Rails.env}/#{Settings.developer_name}/#{model.class.to_s.underscore}/#{model.id}"
-    else
-      "uploads/#{Rails.env}/#{Settings.developer_name}/#{model.class.to_s.underscore}/#{model.id}"
-    end
+    "uploads/#{Rails.env}/#{Settings.developer_name}/#{model.class.to_s.underscore}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
