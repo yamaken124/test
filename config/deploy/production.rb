f13f358e346b66c04ba9182e9eaf2b0403a041be
shinofumijp <electric.spanish.shinofumi.335@gmail.com>
2015-03-18 10:45:05 +0900

    Implement capistrano for production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,8 @@ set :unicorn_rack_env, 'production'
 set :bundle_without,  [:development, :test, :heroku_staging, :staging]
 
 set :application_server_addrs, [
-  ''
+  '54.199.149.61',
+  '54.178.237.8'
 ]
 
 set :cron_server_addr, '54.64.23.242'
