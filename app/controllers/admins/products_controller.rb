d14bfa63b653ede25ef19a14a0cad8d5fae82532
otani shinji <otanishinji@otani-no-MacBook-Air.local>
2015-03-19 13:33:17 +0900

    Make it readable on product_controller

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -19,8 +19,7 @@ class Admins::ProductsController < Admins::BaseController
     begin
       ActiveRecord::Base.transaction do
         raise if ProductsTaxon.without_products_taxon?(params)
-        attributes_params = set_attributes_params
-        @product = Product.new(attributes_params)
+        @product = Product.new(attribute_params)
         @product.save!
       end
       redirect_to admins_product_path(id: @product.id)
@@ -37,10 +36,9 @@ class Admins::ProductsController < Admins::BaseController
   def update
     begin
       ActiveRecord::Base.transaction do
-        attributes_params = set_attributes_params
-        raise if invalid_products_taxons_attributes?(attributes_params)
+        raise if invalid_products_taxons_attributes?(attribute_params)
         ProductsTaxon.create_products_taxon(params)
-        @product.update!(attributes_params)
+        @product.update!(attribute_params)
       end
       redirect_to admins_product_path(params[:id])
     rescue
@@ -56,18 +54,19 @@ class Admins::ProductsController < Admins::BaseController
 
   private
 
-    def set_attributes_params
+    def attribute_params
+      return @attribute_params if @attribute_params
       products_taxons_attributes = ProductsTaxon.set_products_taxons_attributes(params)
       attributes = params.require(:product).permit(:name, :description, :is_valid_at, :is_invalid_at)
-      attributes.merge(products_taxons_attributes)
+      @attribute_params = attributes.merge(products_taxons_attributes)
     end
 
     def set_product
       @product = Product.find(params[:id])
     end
 
-    def invalid_products_taxons_attributes?(attributes_params)
-      attributes_params[:products_taxons_attributes].blank?
+    def invalid_products_taxons_attributes?(attribute_params)
+      attribute_params[:products_taxons_attributes].blank?
     end
 
     def set_leaf_taxons
