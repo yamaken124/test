f4a40cf199e08829f4c917a11ffc7cc8e547ce69
otani shinji <otanishinji@otani-no-MacBook-Air.local>
2015-03-21 05:07:24 +0900

    Use awsome_nested_attributes

--- app/controllers/users/taxons_controller.rb
+++ app/controllers/users/taxons_controller.rb
@@ -1,17 +1,12 @@
 class Users::TaxonsController < Users::BaseController
 
-  def index
-    @taxons = Taxon.where(parent_id: nil)
-  end
-
   def show
-    @taxon = Taxon.where(id: params[:id]).first
-    @nested_taxons = Taxon.where(parent_id: @taxon.id)
-    if @nested_taxons.present?
-      @products = Product.none
-    else
+    @taxon = Taxon.find(params[:id])
+    if @taxon.leaf?
       @products = Product.where(id: ProductsTaxon.where(taxon_id: @taxon.id).pluck(:product_id))
       .active.having_images_and_variants.where(id: Variant.active.pluck(:product_id)).page(params[:page]).includes(:images)
+    else
+      @products = Product.none
     end
   end
 end
