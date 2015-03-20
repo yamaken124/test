b2c5e3deb54977119a10420001ad798b6793a5d7
otani shinji <otanishinji@otani-no-MacBook-Air.local>
2015-03-20 19:13:02 +0900

    Modify bad codes

--- app/models/products_taxon.rb
+++ app/models/products_taxon.rb
@@ -28,7 +28,7 @@ class ProductsTaxon < ActiveRecord::Base
   end
 
   def self.create_products_taxon(product_id, taxon_id)
-    if taxon_id.present?
+    if taxon_id.present? && product_id.present?
       ProductsTaxon.new(product_id: product_id, taxon_id: taxon_id).save!
     end
   end
@@ -36,21 +36,19 @@ class ProductsTaxon < ActiveRecord::Base
   private_class_method
 
     def self.filter_attributes(params, products_taxons_attributes)
-      cnt = 0
-      0.upto(params[:product][:products_taxons_attributes].size-1){|i|
+      params[:product][:products_taxons_attributes].size.times { |i|
         products_taxons_params = params[:product][:products_taxons_attributes].require(i.to_s).permit(:taxon_id, :id)
         if products_taxons_params[:taxon_id].blank?
-          delete_products_taxon(params,i)
+          delete_products_taxon(params[:product][:products_taxons_attributes][i.to_s][:id])
         else
-          products_taxons_attributes[:products_taxons_attributes][cnt.to_s] = products_taxons_params
-          cnt += 1
+          products_taxons_attributes[:products_taxons_attributes][i.to_s] = products_taxons_params
         end
       }
       products_taxons_attributes
     end
 
-    def self.delete_products_taxon(params,i)
-      destroyed_products_taxon = ProductsTaxon.find(params[:product][:products_taxons_attributes][i.to_s][:id])
+    def self.delete_products_taxon(products_taxon_id)
+      destroyed_products_taxon = ProductsTaxon.find(products_taxon_id)
       destroyed_products_taxon.destroy!
     end
 
