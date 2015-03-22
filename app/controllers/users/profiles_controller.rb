23a6ec6bfda59149977b261b41106bf1fceb4512
otani shinji <otanishinji@otani-no-MacBook-Air.local>
2015-03-22 16:14:45 +0900

    Redirect to continue path in case successfully pass new validation

--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -4,24 +4,25 @@ class Users::ProfilesController < Users::BaseController
 
   def create
     @profile = Profile.new(profile_params)
-    if @profile.save
+    if !profile_params.has_value?('') && @profile.save
       redirect_to_continue_path
     else
+      set_continue
       render :edit
     end
   end
 
   def update
     @profile = Profile.where(user_id: @user.id).first
-    if @profile.present? && @profile.update(profile_params)
+    if @profile.present? && !profile_params.has_value?('') && @profile.update(profile_params)
       redirect_to_continue_path
     else
+      set_continue
       render :edit
     end
   end
 
   def edit
-    @user = User.find(current_user.id)
     @profile = Profile.where(user_id: @user.id).blank? ? Profile.new : Profile.where(user_id: @user.id).first
   end
 
@@ -31,6 +32,7 @@ class Users::ProfilesController < Users::BaseController
     end
 
     def profile_params
+
       params.require(:profile).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :phone).merge(user_id: @user.id)
     end
 
@@ -45,6 +47,8 @@ class Users::ProfilesController < Users::BaseController
     def redirect_to_continue_path
       if params[:continue].present? && params[:continue].include?("credit_cards")
         redirect_to new_profile_credit_card_path
+      elsif params[:continue].present? && params[:continue].include?("checkout/payment")
+        redirect_to checkout_state_path("payment")
       else # params[:continue].include?("account") || params[:continue].blank?
         redirect_to account_path
       end
