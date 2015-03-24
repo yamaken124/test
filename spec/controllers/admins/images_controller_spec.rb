61637e1fdcea6dcc80583d6f38ac3a8cf2b130ab
Shiho Kambara <shihokambara@gmail.com>
2015-03-24 23:27:17 +0900

    Fix spec

--- spec/controllers/admins/images_controller_spec.rb
+++ spec/controllers/admins/images_controller_spec.rb
@@ -12,39 +12,39 @@ RSpec.describe Admins::ImagesController, type: :controller do
   end 
 
   describe 'GET #index' do  
-    before { get :index, :imageable_type => "Variant", variant_id: @variant.id }
+    before { get :index, imageable_type: "Variant", variant_id: @variant.id }
 
     it { expect(response).to render_template :index }
     it { expect(assigns(:images)).to eq [ @image ]}
   end 
 
   describe 'GET #new' do
-    before{ get :new, :imageable_type => "Variant", variant_id: @variant.id }
+    before{ get :new, imageable_type: "Variant", variant_id: @variant.id }
     it { expect(response).to render_template :new }
     it { expect(assigns(:image)).to be_new_record }
   end 
 
   describe 'POST #create' do
     it "saves new image in the database" do
-      expect{ post :create, :imageable_type => "Variant", variant_id: @variant.id, image: attributes_for(:image) }.to change(Image, :count).by(1) 
+      expect{ post :create, imageable_type: "Variant", variant_id: @variant.id, image: attributes_for(:image) }.to change(Image, :count).by(1) 
     end
   end   
 
   describe 'GET #edit' do
-    before{ get :edit, :imageable_type => "Variant", variant_id: @variant.id, id: @image.id }
+    before{ get :edit, imageable_type: "Variant", variant_id: @variant.id, id: @image.id }
     it { expect(assigns(:image)).to eq @image  }
     it { expect(response).to render_template :edit }
   end   
 
   describe 'PATCH #update' do
-    before{ patch :update, :imageable_type => "Variant", variant_id: @variant.id, image: attributes_for(:image), id: @image.id }
+    before{ patch :update, imageable_type: "Variant", variant_id: @variant.id, image: attributes_for(:image), id: @image.id }
     it { expect(assigns(:image)).to eq @image }
   end    
 
   describe 'DELETE #destroy' do
-    before{@image_2 = Image.create(:image=> File.open(File.join(Rails.root, '/spec/fixtures/sample.png')), id: 2, imageable_id: @variant.id, imageable_type: "Variant" )}
+    before{@image_2 = Image.create(image: File.open(File.join(Rails.root, '/spec/fixtures/sample.png')), id: 2, imageable_id: @variant.id, imageable_type: "Variant" )}
     it "deletes" do
-      expect{ delete :destroy, :imageable_type => "Variant", variant_id: @variant.id, id: @image.id}.to change(Image, :count).by(-1)
+      expect{ delete :destroy, imageable_type: "Variant", variant_id: @variant.id, id: @image.id }.to change(Image, :count).by(-1)
     end 
   end   
 end
