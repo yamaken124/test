ef81285258a52434ad877c2a959de6c3d8226d23
Shiho Kambara <shihokambara@gmail.com>
2015-03-26 16:25:26 +0900

    Fix indent

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -21,8 +21,8 @@ class Admins::ProductsController < Admins::BaseController
         @product = Product.new(attribute_params)
         @product.save!
       end
-        redirect_to admins_product_variants_path(product_id: @product.id)
-        flash[:notice] = "#{@product.name}が保存されました"
+      redirect_to admins_product_variants_path(product_id: @product.id)
+      flash[:notice] = "#{@product.name}が保存されました"
     rescue
       set_new_product
       set_leaf_taxons
@@ -48,41 +48,41 @@ class Admins::ProductsController < Admins::BaseController
   end
 
   def destroy
-      ActiveRecord::Base.transaction do
-          @product.update(is_invalid_at: Time.now-1)
-          @product.variants.update_all(is_invalid_at: Time.now-1)
-      end
+    ActiveRecord::Base.transaction do
+      @product.update(is_invalid_at: Time.now-1)
+      @product.variants.update_all(is_invalid_at: Time.now-1)
+    end
     redirect_to admins_products_path
   end
 
   private
 
-    def attribute_params
-      return @attribute_params if @attribute_params.present?
-      products_taxons_attributes = ProductsTaxon.products_taxons_attributes(params)
-      attributes = params.require(:product).permit(:name, :description, :is_valid_at, :is_invalid_at)
-      @attribute_params = attributes.merge(products_taxons_attributes)
-    end
+  def attribute_params
+    return @attribute_params if @attribute_params.present?
+    products_taxons_attributes = ProductsTaxon.products_taxons_attributes(params)
+    attributes = params.require(:product).permit(:name, :description, :is_valid_at, :is_invalid_at)
+    @attribute_params = attributes.merge(products_taxons_attributes)
+  end
 
-    def set_product
-      @product = Product.find(params[:id])
-    end
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
-    def invalid_products_taxons_attributes?(attribute_params)
-      attribute_params[:products_taxons_attributes].blank?
-    end
+  def invalid_products_taxons_attributes?(attribute_params)
+    attribute_params[:products_taxons_attributes].blank?
+  end
 
-    def set_leaf_taxons
-      @leaf_taxons = Taxon.leaves
-    end
+  def set_leaf_taxons
+    @leaf_taxons = Taxon.leaves
+  end
 
-    def set_new_product
-      @product ||= Product.new
-      @product.products_taxons.build
-    end
+  def set_new_product
+    @product ||= Product.new
+    @product.products_taxons.build
+  end
 
-    def without_products_taxon?
-      params[:product][:products_taxons_attributes]["0"][:taxon_id].blank?
-    end
+  def without_products_taxon?
+    params[:product][:products_taxons_attributes]["0"][:taxon_id].blank?
+  end
 
 end
