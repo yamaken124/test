b1612d2623d612387870bde811c60a69ba08f46d
Shiho Kambara <shihokambara@gmail.com>
2015-03-30 17:29:21 +0900

    Change the way to solve n+1

--- app/controllers/users/orders_controller.rb
+++ app/controllers/users/orders_controller.rb
@@ -9,9 +9,8 @@ class Users::OrdersController < Users::BaseController
 
   def index
     single_order_detail_id = Payment.where(user_id: current_user.id).pluck(:single_order_detail_id)
-    @details = SingleOrderDetail.where(id: single_order_detail_id).includes(:address, payment: :shipment).includes(:single_line_items).order(completed_at: :desc).page(params[:page])
+    @details = SingleOrderDetail.where(id: single_order_detail_id).includes(:address, payment: :shipment).includes(single_line_items:[variant: [:price]]).order(completed_at: :desc).page(params[:page])
     variant_ids = SingleLineItem.where(single_order_detail_id: @details.pluck(:id)).pluck(:variant_id)
-    @variants_indexed_by_id = Variant.where(id: variant_ids).includes(:images, :price).index_by(&:id)
     @returned_item_indexed_by_item_id = ReturnedItem.where(user_id: current_user.id).index_by(&:single_line_item_id)
   end
 
