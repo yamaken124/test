982e04a2e0a190d9a8e364fff1728d0a91763b53
shinofumijp <electric.spanish.shinofumi.335@gmail.com>
2015-03-31 17:40:40 +0900

    Implement sign_in_with_email_password

--- spec/controllers/users/oauth/authorizations_controller_spec.rb
+++ spec/controllers/users/oauth/authorizations_controller_spec.rb
@@ -45,4 +45,39 @@ RSpec.describe Users::Oauth::AuthorizationsController, type: :controller do
       it { expect{ post :create, params }.to change(OauthAccessToken, :count).by(0) }
     end
   end
+
+  describe 'POST #sign_in_with_email_password' do
+
+    let(:user) { create(:user) }
+    let(:action) { :sign_in_with_email_password }
+    let(:params) { { user: { email: 'shinozuka@finc.co.jp', password: 'password' } }}
+    context 'signed in' do
+      before { user_sign_in user }
+      it { post action; expect(response).to redirect_to root_path }
+    end
+
+    describe 'User not found' do
+      before do
+        stub_request(:post, "#{Settings.internal_api.finc_app.host}/v1/users/login").to_return(status: 201, body: { access_token: nil }.to_json)
+        post action, params
+      end
+
+      it { expect(response).to render_template :sign_in_with_email_password }
+
+      it 'assigns new user' do
+        expect(assigns(:user)).to be_new_record
+        expect(assigns(:user)).to be_instance_of User
+      end
+    end
+
+    describe 'User found' do
+      let(:oauth_application) { OauthApplication.find_by(consumer_key: Settings.oauth_applications.finc_app.consumer_key) }
+      let!(:oauth_access_token) { create(:oauth_access_token, oauth_application_id: oauth_application.id, user_id: user.id, token: 'access') }
+      it do
+        stub_request(:post, "#{Settings.internal_api.finc_app.host}/v1/users/login").to_return(status: 201, body: { access_token: 'access' }.to_json)
+        post action, params
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
 end
