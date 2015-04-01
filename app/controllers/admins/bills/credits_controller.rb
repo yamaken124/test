f4813d9bb2e6df42aed66199d091340437a4ef8a
Shiho Kambara <shihokambara@gmail.com>
2015-04-01 23:55:49 +0900

    Sort from newest to oldest :credits

--- app/controllers/admins/bills/credits_controller.rb
+++ app/controllers/admins/bills/credits_controller.rb
@@ -1,5 +1,5 @@
 class Admins::Bills::CreditsController < Admins::BaseController
   def index
-    @payments = Payment.where(payment_method_id: PaymentMethod::CreditCard).includes(:payment_method).includes(:user)
+    @payments = Payment.where(payment_method_id: PaymentMethod::CreditCard).includes(:payment_method).includes(:user).order("id DESC")
   end
 end
