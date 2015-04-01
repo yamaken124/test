c9bdf6b0a72f7cb4b68ffbd3cd2f0dd16a8be076
otani shinji <otanishinji@otani-no-MacBook-Air.local>
2015-04-01 18:16:10 +0900

    Add store operators ipass for admin

--- db/fixtures/admins.rb
+++ db/fixtures/admins.rb
@@ -3,5 +3,10 @@
   email: 'info@finc.co.jp',
   password: 'D7nomTrZ1i',
   password_confirmation: 'D7nomTrZ1i',
+},{
+  id: 2,
+  email: 'storeoperator@finc.co.jp',
+  password: '1oKQBHekuK',
+  password_confirmation: '1oKQBHekuK',
 }
 ].each { |s| Admin.seed(s) }
