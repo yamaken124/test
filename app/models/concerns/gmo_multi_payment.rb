54ebbc4304907d3118adcc8720feb5f537b8b195
shinofumijp <electric.spanish.shinofumi.335@gmail.com>
2015-04-03 13:46:09 +0900

    Set production gmo settings

--- app/models/concerns/gmo_multi_payment.rb
+++ app/models/concerns/gmo_multi_payment.rb
@@ -1,5 +1,18 @@
 module GmoMultiPayment
   if Rails.env == "production"
+    Domain = "https://kt01.mul-pay.jp/"
+    SiteID = "mst2000000987"
+    SitePass = "kkrzf9mc"
+    ShopID = "tshop00018367"
+    ShopPass = "kzm7ss7m"
+    Tax = 0.08
+    Check = "CHECK"
+    Auth = "AUTH"
+    Sales = "SALES"
+    Capture = "CAPTURE"
+    Void = "VOID"
+    Return = "RETURN"
+    ReturnNx = "RETURNNX"
   else
     Domain = "https://kt01.mul-pay.jp/"
     SiteID = "tsite00017219"
