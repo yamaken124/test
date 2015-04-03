f9603575a2b96aa593c5e61bdc5b2e093b16c9fc
shinofumijp <electric.spanish.shinofumi.335@gmail.com>
2015-04-03 20:02:37 +0900

    Return {} if rails env development

--- app/models/user/finc_app.rb
+++ app/models/user/finc_app.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
     def self.included(klass)
       klass.class_eval do
         def me_in_finc_app
+          return {} if Rails.env.development?
           if access_token = oauth_access_tokens.first
             ::FincApp.me(access_token.token)
           else
