d0bc9076de76313c67297b74e6974d81c801e827
otani shinji <otanishinji@otani-no-MacBook-Air.local>
2015-04-09 01:38:21 +0900

    Remove unneeded param

--- app/helpers/users/checkouts_helper.rb
+++ app/helpers/users/checkouts_helper.rb
@@ -1,6 +1,6 @@
 module Users::CheckoutsHelper
   def permitted_checkout_attributes
-    [{ payment_attributes: [:gmo_card_seq_temporary, :payment_method_id] }, :used_point, :address_id,]
+    [{ payment_attributes: [:gmo_card_seq_temporary, :payment_method_id] }, :address_id,]
   end
 
   def check_as_default_address(address, default_address)
