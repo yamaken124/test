4314e0e28dfe2482f0c5b977d6b6ff6796852303
shinofumijp <electric.spanish.shinofumi.335@gmail.com>
2015-04-20 19:17:31 +0900

    Add airbrake

---
+++ config/initializers/errbit.rb
@@ -0,0 +1,8 @@
+Airbrake.configure do |config|
+  config.api_key = 'a219f9e6a309b4861a4b835242486338'
+  config.host    = 'errbit-sample-finc.herokuapp.com'
+  config.port    = 80
+  config.secure  = config.port == 443
+  config.environment_name = Rails.env.production? ? "#{Rails.env}:#{`hostname`}" : Rails.env
+  config.development_environments = []
+end
