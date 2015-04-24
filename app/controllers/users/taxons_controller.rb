829989649aaa6ff6bed90a940e18518cbb1c45dc
otani shinji <otanishinji@otani-no-MacBook-Air.local>
2015-04-24 11:23:38 +0900

    Fix taxon non-display bug

--- app/controllers/users/taxons_controller.rb
+++ app/controllers/users/taxons_controller.rb
@@ -21,7 +21,7 @@ class Users::TaxonsController < Users::BaseController
     def set_products(available_variants)
       selected_product_id = ProductsTaxon.where(taxon_id: @taxon.id).pluck(:product_id)
 
-      displayed_product_ids = Product.available.ids & current_user.shown_product_ids
+      displayed_product_ids = Product.available.try(:ids) & current_user.shown_product_ids
       @products = Product.active.where(id: (displayed_product_ids & selected_product_id) ).page(params[:page])
     end
 
