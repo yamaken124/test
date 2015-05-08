9ffe8c928d2c0533fe2b9e3ece71bfc1c11be3db
shinjifinc <shinji.otani.fb@gmail.com>
2015-05-08 21:51:58 +0900

    Integrate by using products helper

--- app/controllers/users/products_controller.rb
+++ app/controllers/users/products_controller.rb
@@ -1,19 +1,14 @@
 class Users::ProductsController < Users::BaseController
 
   include Users::OneClickOrdersHelper
+  include Users::ProductsHelper
 
   before_action :set_product, only: [:show, :description, :show_one_click]
   before_action :available_quantity, only: [:show, :description, :show_one_click]
   before_action :redirect_to_profile_if_without_any, only: [:post_one_click_order]
 
   def index
-    set_products(Variant.available)
-
-    displayed_variant_ids = Variant.available.ids & Variant.where(product_id: @products.ids).ids
-    @single_variants = Variant.where(id: displayed_variant_ids).single_order.includes(:price)
-    @subscription_variants = Variant.where(id: displayed_variant_ids).subscription_order.includes(:price)
-    # set_prices(displayed_variant_ids)
-    top_image(@single_variants)
+    top_products
   end
 
   def show
@@ -61,22 +56,22 @@ class Users::ProductsController < Users::BaseController
       reject_invalid_product
     end
 
-    def set_products(available_variants)
-      displayed_product_ids = Product.available.try(:ids) & current_user.shown_product_ids
-      @products = Product.where(id: displayed_product_ids).page(params[:page])
-    end
+    # def set_products(available_variants)
+    #   displayed_product_ids = Product.available.try(:ids) & current_user.shown_product_ids
+    #   @products = Product.where(id: displayed_product_ids).page(params[:page])
+    # end
 
-    def set_prices(variant_ids)
-      single_variants = Variant.where(id: variant_ids).single_order
-      @single_prices_indexed_by_variant_id = Price.where(variant_id: single_variants.ids).index_by(&:variant_id)
+    # def set_prices(variant_ids)
+    #   single_variants = Variant.where(id: variant_ids).single_order
+    #   @single_prices_indexed_by_variant_id = Price.where(variant_id: single_variants.ids).index_by(&:variant_id)
 
-      subscription_variants = Variant.where(id: variant_ids).subscription_order
-      @subscription_prices_indexed_by_variant_id = Price.where(variant_id: subscription_variants.ids).index_by(&:variant_id)
-    end
+    #   subscription_variants = Variant.where(id: variant_ids).subscription_order
+    #   @subscription_prices_indexed_by_variant_id = Price.where(variant_id: subscription_variants.ids).index_by(&:variant_id)
+    # end
 
-    def top_image(variants)
-      @images = Image.where(id: VariantImageWhereabout.top.where(variant_id: variants.ids).pluck(:image_id)).order('position ASC')
-    end
+    # def top_image(variants)
+    #   @images = Image.where(id: VariantImageWhereabout.top.where(variant_id: variants.ids).pluck(:image_id)).order('position ASC')
+    # end
 
     def available_quantity
       @available_quantity = Array(1..Product::AvailableQuantity)
--- app/controllers/users/taxons_controller.rb
+++ app/controllers/users/taxons_controller.rb
@@ -1,46 +1,13 @@
 class Users::TaxonsController < Users::BaseController
 
+  include Users::ProductsHelper
+
   def show
     @taxon = Taxon.find(params[:id])
-    if @taxon.leaf?
-      set_products(Variant.available)
 
-      displayed_variant_ids = Variant.available.ids & Variant.where( product_id: (current_user.shown_product_ids & @products.ids) ).ids
-      @single_variants = Variant.where(id: displayed_variant_ids).single_order.includes(:price)
-      @subscription_variants = Variant.where(id: displayed_variant_ids).subscription_order.includes(:price)
-
-      # set_prices(displayed_variant_ids)
-      top_image(@single_variants)
-
-    else
-      @products = Product.none
-    end
-
+    @taxon.leaf? ? top_products : @products = Product.none
   end
 
   private
-
-    def set_products(available_variants)
-      selected_product_id = ProductsTaxon.where(taxon_id: @taxon.id).pluck(:product_id)
-
-      displayed_product_ids = Product.available.try(:ids) & current_user.shown_product_ids
-      @products = Product.where(id: (displayed_product_ids & selected_product_id) ).page(params[:page])
-    end
-
-    def set_prices(variant_ids)
-      single_variants = Variant.where(id: variant_ids).single_order
-      @single_prices_indexed_by_variant_id = Price.where(variant_id: single_variants.ids).index_by(&:variant_id)
-
-      subscription_variants = Variant.where(id: variant_ids).subscription_order
-      @subscription_prices_indexed_by_variant_id = Price.where(variant_id: subscription_variants.ids).index_by(&:variant_id)
-    end
-
-    def set_images(variant_ids)
-      @images = Image.where(imageable_id: variant_ids, imageable_type: 'Variant').index_by(&:imageable_id) #TODO 両方写真があるとき！！
-    end
-
-    def top_image(variants)
-      @images = Image.where(id: VariantImageWhereabout.top.where(variant_id: variants.ids).pluck(:image_id)).order('position ASC')
-    end
 
 end
